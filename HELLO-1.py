514b1a4b40c1ad32abd0df3b4073e63796bf23ca
Jidouman <hamadjibril29@gmail.com>
2025-03-14 21:46:27 +0100

    I deleted the "Print()" at the bottom of the page

--- HELLO-1.py
+++ HELLO-1.py
@@ -274,6 +274,3 @@ Happy Learning!
 # Footer
 st.write("---")
 st.write("FCS Streamlit Tutorial • Designed with ❤️ for Bachelor Students in Business Administration")
-
-print("I Love FCS")
-print("BB")

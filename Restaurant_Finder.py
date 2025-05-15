dc65e62e3957789decc7678b5e4db068ec4bef62
Jidouman <hamadjibril29@gmail.com>
2025-05-15 02:25:59 +0200

    Documentation

--- Restaurant_Finder.py
+++ Restaurant_Finder.py
@@ -1,5 +1,5 @@
-import streamlit as st
-import pandas as pd
+import streamlit as st # Core Streamlit library for building the UI
+import pandas as pd # Data manipulation via DataFrames
 from streamlit_js_eval import streamlit_js_eval  # For geolocation
 import requests
 import math # For distance calculation
